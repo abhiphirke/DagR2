131c595dc77f2ce89a4c2d1a4f6a13a4cc4b7352
Phirke Abhijeet <abhijeet.phirke@tieto.com>
2019-09-20 11:54:06 +0530

    My new repo

---
+++ precache-manifest.49b7fff11e23c3ba19496448b8e7a921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afe51960f4c3f9d5e7a2698ed0b75eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f95c023fa7082eeb292f",
+    "url": "./static/css/2.4146db7d.chunk.css"
+  },
+  {
+    "revision": "a0ffc8d87446dc931350",
+    "url": "./static/css/main.cc5d88d4.chunk.css"
+  },
+  {
+    "revision": "f95c023fa7082eeb292f",
+    "url": "./static/js/2.baf30317.chunk.js"
+  },
+  {
+    "revision": "a0ffc8d87446dc931350",
+    "url": "./static/js/main.b932cd31.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
