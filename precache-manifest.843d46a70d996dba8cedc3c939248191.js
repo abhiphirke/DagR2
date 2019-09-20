b55fde8a3a659d9d4b5c9abf21bdfee3717b2b07
Phirke Abhijeet <abhijeet.phirke@tieto.com>
2019-09-20 12:49:55 +0530

    test overview 3

---
+++ precache-manifest.843d46a70d996dba8cedc3c939248191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1302275658e03738792a689a4029f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4f78347951325863e4d",
+    "url": "./static/css/2.4146db7d.chunk.css"
+  },
+  {
+    "revision": "85d3574e5c2ececf8657",
+    "url": "./static/css/main.e94b4e61.chunk.css"
+  },
+  {
+    "revision": "b4f78347951325863e4d",
+    "url": "./static/js/2.2e5edc6c.chunk.js"
+  },
+  {
+    "revision": "85d3574e5c2ececf8657",
+    "url": "./static/js/main.d2314f8d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.759fe9c27ce5db5263643a7a11cb8d21.js"
+  "./precache-manifest.843d46a70d996dba8cedc3c939248191.js"
 );
 
 self.addEventListener('message', (event) => {
